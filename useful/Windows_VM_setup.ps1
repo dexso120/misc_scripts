a0c89e96de8b9d9fe156c1a6e595fdea5a213726
dscy120 <chingyiuso120@gmail.com>
2025-10-04 15:16:14 +0100

    Update Windows_VM_setup script
    Added modification to local group policy for disabling Windows Defender and Auto Updates.
    
    Also added auto download of psexec.

--- useful/Windows_VM_setup.ps1
+++ useful/Windows_VM_setup.ps1
@@ -25,5 +25,26 @@ If (!(Test-Path $theme_path_cu)){
 New-ItemProperty -Path $theme_path_lm -Name $name -Value $value -PropertyType DWORD -Force
 New-ItemProperty -Path $theme_path_cu -Name $name -Value $value -PropertyType DWORD -Force
 
+# Disable Windows Auto Update
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1
+
+# Disable Windows Defender
+Set-MpPreference -DisableRealtimeMonitoring $true
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
+
+# Download PsExec
+$tools_path = 'C:\Tools\'
+$pstools_path = 'C:\Tools\pstools'
+New-Item -ItemType Directory -Path $tools_path
+New-Item -ItemType Directory -Path $pstools_path
+Invoke-WebRequest https://download.sysinternals.com/files/PSTools.zip -outfile $tools_path'PsTools.zip'
+Expand-Archive -Path $tools_path'PsTools.zip' -DestinationPath $pstools_path
+
+# Guide to disable auto shutdown
+Write-Host 'To disable auto shutdown on trial version.'
+Write-Host 'Run "Psexec -i -s cmd.exe" and run "services.msc".'
+Write-Host 'Go to "Windows Licensing Monitoring Service" > "Disable".'
+Write-Host 'Then restart device.'
+
 # Reboot to apply changes
 # Restart-Computer
