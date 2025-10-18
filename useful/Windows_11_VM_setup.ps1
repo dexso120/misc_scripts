ddc1ae248c3089ad9c18e7123ba0688fa9e1b6be
dscy120 <chingyiuso120@gmail.com>
2025-10-18 15:07:17 +0100

    Disable screen timeout

--- useful/Windows_11_VM_setup.ps1
+++ useful/Windows_11_VM_setup.ps1
@@ -26,7 +26,7 @@ New-ItemProperty -Path $theme_path_lm -Name $name -Value $value -PropertyType DW
 New-ItemProperty -Path $theme_path_cu -Name $name -Value $value -PropertyType DWORD -Force
 
 # Disable Windows Auto Update
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1 -Force
 
 # Disable Windows Defender
 Set-MpPreference -DisableRealtimeMonitoring $true
@@ -35,13 +35,21 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Nam
 # Download PsExec
 $tools_path = 'C:\Tools\'
 $pstools_path = 'C:\Tools\pstools'
-New-Item -ItemType Directory -Path $tools_path
-New-Item -ItemType Directory -Path $pstools_path
-Invoke-WebRequest https://download.sysinternals.com/files/PSTools.zip -outfile $tools_path'PsTools.zip'
-Expand-Archive -Path $tools_path'PsTools.zip' -DestinationPath $pstools_path
+If (Test-Path $pstools_path){
+	New-Item -ItemType Directory -Path $tools_path
+	New-Item -ItemType Directory -Path $pstools_path
+	Invoke-WebRequest https://download.sysinternals.com/files/PSTools.zip -outfile $tools_path'PsTools.zip'
+	Expand-Archive -Path $tools_path'PsTools.zip' -DestinationPath $pstools_path -Force
+}
 
-# Diable auto shutdown for Windows 11 trial
-Start-Process -FilePath "cmd.exe" -Verb RunAs -ArgumentList '/c c: && cd / && cd Tools\pstools && psexec -accepteula -i -s "reg add HKLM\SYSTEM\CurrentControlSet\Services\WLMS /v Start /t REG_DWORD /d 4 /f"'
+# Disable auto shutdown for Windows 11 trial
+Start-Process -FilePath "cmd.exe" -Verb RunAs -ArgumentList '/c c: && cd / && cd Tools\pstools && psexec -accepteula -i -s "reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WLMS /v Start /t REG_DWORD /d 4 /f"'
+
+# Allow to modify Power Options
+Start-Process -FilePath "cmd.exe" -Verb RunAs -ArgumentList '/c c: && cd / && cd Tools\pstools && psexec -accepteula -i -s "reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7 /v Attributes /t REG_DWORD /d 2 /f"'
+
+# Disable screen timeout
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name InactivityTimeoutSecs -Value 0 -PropertyType DWORD -Force
 
 # Print info
 Write-Host "All configuration applied. Please restart computer."
